42b4e70e9865b85b2b25f83601e60d210d863ed9
mahendrakanchu <mahendrakanchu2003@gmail.com>
2025-06-27 12:23:36 +0530

    Initial commit

---
+++ backend/routes/urlRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { shortenUrl,redirectUrl } from '../controllers/urlcontroller.js';
+const router=express.Router();
+
+router.post("/shorten",shortenUrl);
+router.get('/shorten',(req,res)=>{
+    console.log('hello');
+    res.send('hello');
+});
+router.get('/:shortUrl',redirectUrl);
+
+export default router;
